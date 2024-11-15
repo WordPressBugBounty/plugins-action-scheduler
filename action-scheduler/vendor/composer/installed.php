2afb07a9829741a92331165b94c75888eda05429
Sean Murphy <sean@iamseanmurphy.com>
2024-11-15 02:00:08 +0000

    3.9.0

--- action-scheduler/vendor/composer/installed.php
+++ action-scheduler/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'woocommerce/action-scheduler',
-        'pretty_version' => 'dev-release/3.8.2',
-        'version' => 'dev-release/3.8.2',
-        'reference' => '2bc91d88fdbc2c07ab899cbb56b983e11e62cf69',
+        'pretty_version' => 'dev-release/3.9.0',
+        'version' => 'dev-release/3.9.0',
+        'reference' => '90b98e6fe97d455679b1d288f050cad8f6f79771',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'woocommerce/action-scheduler' => array(
-            'pretty_version' => 'dev-release/3.8.2',
-            'version' => 'dev-release/3.8.2',
-            'reference' => '2bc91d88fdbc2c07ab899cbb56b983e11e62cf69',
+            'pretty_version' => 'dev-release/3.9.0',
+            'version' => 'dev-release/3.9.0',
+            'reference' => '90b98e6fe97d455679b1d288f050cad8f6f79771',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
